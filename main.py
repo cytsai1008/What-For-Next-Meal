c85afa99546b1841fe1fc45bbc85b0f1265bbfab
CYTsai <cytsai1008@gmail.com>
2022-01-14 15:55:27 +0800

    db folder detection

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 if not os.path.exists('Log'):
     os.mkdir('Log')
+if not os.path.exists('db'):
+    os.mkdir('db')
 handler = logging.FileHandler(filename='Log/discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
