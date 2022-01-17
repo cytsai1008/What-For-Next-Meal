8b7dd43439cebb4d8d81dd281a60571bc7b1eb98
CYTsai <47322522+cytsai1008@users.noreply.github.com>
2022-01-17 19:27:43 +0800

    Just some small thing I trust you can understand

--- tool_function.py
+++ tool_function.py
@@ -1,18 +1,18 @@
 import json
 
 
-def read_json(filename):
+def read_json(filename) -> dict:
     with open(filename, 'r') as f:
         data = json.load(f)
     return data
 
 
-def write_json(filename, data):
+def write_json(filename, data) -> None:
     with open(filename, 'w') as f:
         json.dump(data, f)
 
 
-def check_json(filename):
+def check_json(filename) -> dict:
     try:
         with open(filename, 'r') as f:
             data = json.load(f)
@@ -21,23 +21,24 @@ def check_json(filename):
     return data
 
 
-def check_args_zero(args, arg_list):
+def check_args_zero(args, arg_list) -> bool:
     return all(arg in args for arg in arg_list)
 
 
-def id_check(message):
+def id_check(self) -> str:
     try:
-        server_id = str(message.guild.id)
+        server_id = str(self.guild.id)
     except:
-        server_id = str(message.author.id)
+        server_id = str(self.author.id)
     return server_id
 
 
-def check_args_one(args):
+def check_args_one(args) -> bool:
     return args is not type(None)
+    # return False if args is type(None)
 
 
-def check_dict_data(data):
+def check_dict_data(data:dict) -> bool:
     try:
         print(data)
     except KeyError:
@@ -46,16 +47,16 @@ def check_dict_data(data):
         return True
 
 
-def get_duplicate_counters(old, new):
+def get_duplicate_counters(old:int, new:int) -> int:
     return new - old
 
 
-def check_duplicate_data(existing_data, new_data):
+def check_duplicate_data(existing_data, new_data:list) -> list:
     del_key = []
     for i in range(len(existing_data)):
-        for j in range(len(new_data)):
-            if existing_data[i] == new_data[j]:
-                del_key.append(new_data[j])
+        for new_datum in new_data:
+            if existing_data[i] == new_datum:
+                del_key.append(new_datum)
     return del_key
 
 # TODO: time commands function
